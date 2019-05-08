5c3396a91384f173d837eff63da037ab027609f3
Roger Dunn <rrdunn@gmail.com>
2019-05-07 20:17:02 -0700

    Complete miniapp2

--- challenge_2/client/app.js
+++ challenge_2/client/app.js
@@ -1,2 +1,64 @@
 
+class Page {
+    constructor() {
+    }
 
+    sendAjaxPost(text) {
+        var url = 'http://localhost:3000/';
+        $.ajax({
+            type: "POST",
+            url: url,
+            data: {input: text},
+            success: (data) => {
+                document.getElementById('output').value = "";
+                document.getElementById('output').value = data;
+                let csvContent = "data:text/csv;charset=utf-8," + data;
+                csvContent = encodeURI(csvContent);
+                var link = document.createElement('a');
+                link.setAttribute('href', csvContent);
+                link.setAttribute('download', 'flattenedData.csv');
+                document.body.appendChild(link);
+                link.click();
+            }
+        });
+    }
+
+    addTextInputListener() {
+        document.getElementById("form1").addEventListener('submit', this.handleTextSubmit);
+    }
+
+    addFileSelectListener() {
+        document.getElementById("file").addEventListener('change', this.handleFileSelect);
+    }
+
+    handleTextSubmit(event) {
+        event.preventDefault();
+        var text = document.getElementsByName('input')[0].value;
+        page.sendAjaxPost(text);
+        document.getElementsByName('input')[0].value = '';
+    }
+
+
+    handleFileSelect(event) {
+        event.preventDefault();
+        var f = event.target.files[0];
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            page.sendAjaxPost(e.target.result);
+        }
+        reader.readAsText(f);
+        document.getElementById("file").value = null;
+        
+    }
+
+
+ 
+}
+
+var page;
+
+window.onload = function(){
+ page = new Page();
+ page.addFileSelectListener();
+ page.addTextInputListener();
+}
--- challenge_2/server.js
+++ challenge_2/server.js
@@ -1,2 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const port = 3000;
+
+app.use(express.static('./client'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/', (req, res) => {
+    let flattenedData = flattenObj(JSON.parse(req.body.input));
+    res.setHeader('Content-disposition', 'attachment; filename=flattenedData.csv');
+    res.set('Content-Type', 'text/csv');
+    res.status(200).send(flattenedData);
+});
 
 
+var flattenObj = (body) => {
+    var output = "";
+    var keys = Object.keys(body);
+    keys.pop();
+    output += keys.join(",") +'\n';
+    var traverseChildren = (parent) => {
+        if (parent === null) {
+            return;
+        }
+        var person = [parent.firstName, parent.lastName, parent.county, parent.city, parent.role, parent.sales];
+        output += person.join(",") + '\n';
+        for (let i = 0; i < parent.children.length; i++) {
+            let child = parent.children[i];
+            traverseChildren(child);
+        }
+    };
+    traverseChildren(body);
+    return output;
+};
+
+app.listen(port, () => console.log(`App listening on port ${port}!`))
